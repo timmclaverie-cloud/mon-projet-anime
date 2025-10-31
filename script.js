063b2672c9a2abb936593c0ee11384fa22e18e69
timmclaverie-cloud <timmclaverie@gmail.com>
2025-10-31 20:40:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,6 @@ const [ $logo ] = utils.$('.logo.js');
 const [ $button ] = utils.$('button');
 let rotations = 0;
 
-import { createTimer } from 'animejs';
-
-const [ $time, $count ] = utils.$('.value');
-
-createTimer({
-  duration: 1000,
-  loop: true,
-  frameRate: 30,
-  onUpdate: self => $time.innerHTML = self.currentTime,
-  onLoop: self => $count.innerHTML = self._currentIteration
-});
-
 // Created a bounce animation loop
 animate('.logo.js', {
   scale: [
